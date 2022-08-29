57b0e2422f27889899c61e2d9f17eb4cecdc5706
Josh Sparks <sparksj404@gmail.com>
2022-08-29 12:27:34 -0400

    video_model.js with embedded link

--- models/video_model.js
+++ models/video_model.js
@@ -1,16 +1,16 @@
 const videos = [
     {
-        url:'https://www.youtube.com/watch?v=SXxEEtDhXks&ab_channel=BruceWayneTV',
+        url:"https://www.youtube.com/embed/SXxEEtDhXks",
         title: 'Synthwave / Spacewave Music Productivity',
         uploader: 'Josh Sparks'
     },
     {
-        url: 'https://www.youtube.com/watch?v=q9vdHoxuaQc&ab_channel=AmazonPrimeVideoUK',
+        url: "https://www.youtube.com/embed/q9vdHoxuaQc",
         title: 'The Grand Tour Presents: A Scandi Flick',
         uploader: 'Amazon Prime'
     },
     {
-        url: 'https://www.youtube.com/watch?v=-JTCXCzm5Lc&list=PLxpnMQNf05If_RQcRcfn-EnLjRAB3sPAw&ab_channel=RegularCars',
+        url: "https://www.youtube.com/embed/-JTCXCzm5Lc",
         title: '2007 Porsche 911 GT3 RS SharkWerks 3.9L: RCR',
         uploader: 'RCR'
     }
